7d29f11f596ad4954e1234359a014f33a054c904
elijah <elaahhaq123@gmail.com>
2021-02-15 11:30:56 +0000

    Add other files

---
+++ library_system/library_system/doctype/library_transactions/test_library_transactions.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021, Elijah and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+# import frappe
+import unittest
+
+class TestLibraryTransactions(unittest.TestCase):
+	pass
